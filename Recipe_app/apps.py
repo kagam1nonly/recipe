608a785479626a0997ef9c8861367c5c543fd59e
James Piastro <78834746+kagam1nonly@users.noreply.github.com>
2023-09-23 03:42:31 +0800

    Delete Recipe_app directory

--- Recipe_app/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class RecipeAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Recipe_app'
-    verbose_name = 'Recipe'
--- Recipe_app/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Recipe_app
-
-class RecipeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Recipe_app
-        fields = '__all__'
