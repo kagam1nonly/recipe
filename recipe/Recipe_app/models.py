7b870f09895625bed33a29a4c04c01012bd298cd
James Piastro <78834746+kagam1nonly@users.noreply.github.com>
2023-09-24 00:34:12 +0800

    Update models.py
    Removed AvailablePrimaryKey Class Added New Ingredient Model

--- recipe/Recipe_app/models.py
+++ recipe/Recipe_app/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100, blank=False, default='')
+    description = models.TextField(max_length=255, blank=False, default='')
+    cost = models.DecimalField(max_digits=10, decimal_places=2, blank=False, default=0.00)
+
+    def __str__(self):
+        return self.name
+
 class Recipe_app(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=100, blank=False, default='')
@@ -8,9 +16,3 @@ class Recipe_app(models.Model):
 
     class Meta:
         ordering = ['created']
-
-class AvailablePrimaryKey(models.Model):
-    next_id = models.IntegerField(unique=True)
-
-    def __str__(self):
-        return str(self.next_id)
