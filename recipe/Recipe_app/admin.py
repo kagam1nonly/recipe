bd7e9b7761f11b6fa69ae7a1ba810ea83b110236
James Piastro <78834746+kagam1nonly@users.noreply.github.com>
2023-09-24 00:35:04 +0800

    Update admin.py
    Ingredient Model Added in Admin Panel

--- recipe/Recipe_app/admin.py
+++ recipe/Recipe_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Recipe_app
+from .models import Recipe_app, Ingredient
 
 # Register your models here.
 admin.site.register(Recipe_app)
+admin.site.register(Ingredient)
