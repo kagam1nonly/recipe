46f85c1da4ea6333ce368f4f5a8389fb2a2a2f1c
James Piastro <78834746+kagam1nonly@users.noreply.github.com>
2023-09-24 00:33:16 +0800

    Update serializers.py
    New Serializer IngredientSerializer

--- recipe/Recipe_app/serializers.py
+++ recipe/Recipe_app/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Recipe_app
+from .models import Recipe_app, Ingredient
 
 class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe_app
+        fields = '__all__'
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
         fields = '__all__'
